3dd008aa2b8a5a93c7878264af2da5160684428d
pswalia2u <pswalia2u@gmail.com>
2021-09-22 01:14:11 +0530

    Update bad_chars.py

--- bad_chars.py
+++ bad_chars.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python3
+from __future__ import print_function
+listRem = "".split("\\x")
 for x in range(1, 256):
-  print("\\x" + "{:02x}".format(x), end='')
+    if "{:02x}".format(x) not in listRem:
+        print("\\x" + "{:02x}".format(x), end='')
 print()

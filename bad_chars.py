9948c1deb45b532382a900b8fcbfd333ed94b893
pswalia2u <pswalia2u@gmail.com>
2021-09-23 01:43:40 +0530

    Update bad_chars.py

--- bad_chars.py
+++ bad_chars.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from __future__ import print_function
+#listRem = "\\x04\\x76\\x99\xf3".split("\\x")
 listRem = "".split("\\x")
 for x in range(1, 256):
     if "{:02x}".format(x) not in listRem:
